3ae5a572104252eacdee902abfeeb461248d4ace
Cyril442 <cyril.cieslak@gmail.com>
2019-08-06 10:54:04 +0200

    j

--- app/src/androidTest/java/androidviakotlin/livedatainmainactivity/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/androidviakotlin/livedatainmainactivity/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package cyril.cieslak.livedatainmainactivity
+package androidviakotlin.livedatainmainactivity
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("cyril.cieslak.livedatainmainactivity", appContext.packageName)
+        assertEquals("androidviakotlin.livedatainmainactivity", appContext.packageName)
     }
 }
